e5badd1b108ab54c1008e04d41fbd47a5268bd9e
Juan <juan.gallegomesa@gmail.com>
2020-10-15 09:37:58 -0500

    se cera hola mundo en php

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<?php 
+ 
+    $nombreUsuario="Pedro Coral";
+    $edad=40;
+    $estatura=1.80;
+    $nombreUsuario="Juan Perez";
+
+    define("PI",3.141516);
+
+    echo("HOLA MUNDO ".$nombreUsuario." ".PI);
+
+?>
