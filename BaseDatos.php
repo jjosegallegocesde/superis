719be3059445833a11dfe1a477af2fb831960052
Juan <juan.gallegomesa@gmail.com>
2020-11-17 09:52:31 -0500

    SE CREA RUTINA PARA AGREGAR DATOS

---
+++ BaseDatos.php
@@ -0,0 +1,62 @@
+<?php 
+
+class BaseDatos{
+
+//DATOS/VARIABLES/ATRIBUTOS
+public $usuarioBD="root";
+public $passwordBD="";
+
+
+//CONSTRUCTOR FUNCION ESPECIAL
+public function __construct(){}
+
+
+//ACCIONES/FUNCIONES/METODOS
+public function conectarBD(){
+
+    //1. DEFINIR EL DSN(info generica de mi BD)
+    //gestorBD:host=nombreServidor;dbname=nombreBD
+    $infoBD="mysql:host=localhost;dbname=tiendais";
+
+    //2. Revisar(intentar) si hay conexión con la BD
+    try{
+
+        //3. UTILIZAR A PDO(crear un objeto de la clase PDO)
+        $conexionBD=new PDO($infoBD, $this->usuarioBD, $this->passwordBD);
+        return($conexionBD);
+
+    }catch(PDOException $error){
+        //3.1 Manejo del error
+        echo($error->getMessage());
+    }
+
+}
+
+public function agregarDatos($consultaSQL){
+
+    //1. Establecer una conexión con la BD
+    $conexionBD=$this->conectarBD();
+
+    //2. Peparar la consulta
+    $consultaInsertarDatos=$conexionBD->prepare($consultaSQL);
+
+    //3. Ejecutar la consulta
+    $resultado=$consultaInsertarDatos->execute();
+
+    //4. Verificar el resultado
+    if($resultado){
+        echo("se agrego el registro con éxito");
+    }else{
+        echo("Error agregando el registro");
+    }
+    
+}
+
+
+
+
+}
+
+
+
+?>
---
+++ registrarUsuarios.php
@@ -0,0 +1,27 @@
+<?php
+
+    include("BaseDatos.php");
+
+    if(isset($_POST["botonEnvio"])){
+        
+        //1. Recibo datos del formulario
+        $nombre=$_POST["nombre"];
+        $apellido=$_POST["apellido"];
+        $descripcion=$_POST["descripcion"];
+        $genero=$_POST["genero"];
+
+        //2. Crear un objeto(COPIA) de la clase BaseDatos
+        $transaccion=new BaseDatos();
+
+        //3.Construir una consulta SQL para insertar datos
+        $consultaSQL="INSERT INTO usuarios(nombre,apellido,descripcion,genero) VALUES ('$nombre','$apellido','$descripcion','$genero')";
+        
+        //4. Utilizar el metodo agregarDatos() de la clase BaseDatos
+        $transaccion->agregarDatos($consultaSQL);
+
+    }
+
+
+
+
+?>
