f59f372f79b22ad0a6cc200bea18541f86205b4d
Juan <juan.gallegomesa@gmail.com>
2020-11-19 11:05:41 -0500

    se crea tabla dinamica

--- listadoUsuarios.php
+++ listadoUsuarios.php
@@ -28,6 +28,32 @@
 
     ?>
 
+    <div class="container">
+        
+
+        <div class="row row-cols-1 row-cols-md-3">
+
+            <?php foreach($usuarios as $usuario): ?>
+
+                <div class="col mb-4">
+                    <div class="card h-100">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $usuario["nombre"] ?></h5>
+                            <p class="card-text"><?= $usuario["descripcion"] ?></p>
+                        </div>
+                    </div>
+                </div>
+                
+            <?php endforeach ?>
+            
+
+        </div>
+    
+    </div>
+
+
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> 
 </body>
